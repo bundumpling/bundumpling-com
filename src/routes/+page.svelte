c6a6959978d365dbf72dcfeb36417ba77b5db9dc
bundumpling <bunyourdumplings@gmail.com>
2023-12-13 16:08:02 -0500

    add logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,23 @@
-<h1>bundumpling says hi</h1>
+<svelte:head>
+	<title>bundumpling</title>
+</svelte:head>
+
+<header>
+	<a href="/"
+		><img
+			src="logo.png"
+			alt="Brush style text displaying 'bundumpling' between mirrored illustrations of a scallion."
+		/></a
+	>
+</header>
+
+<style>
+	:global(body) {
+		background-color: #ab540e;
+	}
+
+	header {
+		display: flex;
+		justify-content: center;
+	}
+</style>
